d71a8b17260d52fb0915d57b8a3ce97cd79ff3d3
16211994 <cakek433@gmail.com>
2024-12-12 21:14:55 +0300

    Adding rest_framework jwt under installed apps

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -16,9 +16,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'core',
-   
-    'core.user'
+    'core.user',
+    
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
