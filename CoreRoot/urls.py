8e53f77871e821e2b03b6eadfcc529c422f9283d
16211994 <cakek433@gmail.com>
2024-12-13 21:04:57 +0300

    some modification

--- CoreRoot/urls.py
+++ CoreRoot/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
-    path('api/', include(('core.routers', 'core'), namespace="core-api")), 
+    path('admin/', admin.site.urls), 
+    path('api/auth/register', include(('core.routers', 'core'))), 
+    path('api/', include(('core.routers', 'core'), namespace='core-api')), 
 ]
