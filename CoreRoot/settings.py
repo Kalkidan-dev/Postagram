7de7b47e9328b353b284e2d1ad358060ac85225d
16211994 <cakek433@gmail.com>
2024-12-19 21:06:29 +0300

    Add a pagination for better represenation

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -12,6 +12,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+        'PAGE_SIZE': 15,
+
     'DEFAULT_FILTER_BACKENDS':
         ['django_filters.rest_framework.DjangoFilterBackend'],
 }
