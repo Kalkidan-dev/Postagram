81e20b51559f895df0fbe44bd084b6bfa2d599a5
16211994 <cakek433@gmail.com>
2024-12-20 22:29:01 +0300

    add a method to create ang get-queryset

--- core/comment/viewsets.py
+++ core/comment/viewsets.py
@@ -5,3 +5,26 @@ from rest_framework import status
 from core.abstract.viewsets import AbstractViewSet
 from core.comment.models import Comment
 from core.comment.serializers import CommentSerializer
+from core.auth.permissions import UserPermission
+
+class CommentViewSet(AbstractViewSet):
+    http_method_names = ('post', 'get', 'put', 'delete')
+    permission_classes = (UserPermission,)
+    serializer_class = CommentSerializer
+
+    def create(self, request, *args, **kwargs):
+      serializer = self.get_serializer(data=request.data)
+      serializer.is_valid(raise_exception=True)
+      self.perform_create(serializer)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def get_queryset(self):
+        if self.request.user.is_superuser: 
+            return Comment.objects.all()
+        post_pk = self.kwargs['post_pk']
+
+        if post_pk is None:
+          return Http404
+        queryset = Comment.objects.filter(
+          post__public_id=post_pk)
+        return queryset
