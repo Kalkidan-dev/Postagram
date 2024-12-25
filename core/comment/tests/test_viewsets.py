010b2209bcb7afa4cae23e795ff525bc988a4ab4
16211994 <cakek433@gmail.com>
2024-12-25 12:39:25 +0300

    Add a test for create, update and delete comment with authenticated user

--- core/comment/tests/test_viewsets.py
+++ core/comment/tests/test_viewsets.py
@@ -20,4 +20,31 @@ class TestCommentViewSet:
         assert response.data['body'] == comment.body
         assert response.data['author']['id'] == comment.author.public_id.hex
 
+    def test_create(self, client, user, post):
+        data = {
+            "author" : user.public_id.hex,
+            "post" : post.public_id.hex,
+            "body" : "Test Comment Body"
+        }
+        client.force_authenticate(user = user)
+        response = client.post(self.endpoint + str(post.public_id) + "/comment/", data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data ['author'] ['id'] == user.public_id.hex
+        assert response.data['body'] == data["body"]
     
+    def test_update(self, client, user, post, comment):
+        data = {
+            "author" : user.public_id.hex,
+            "post" : post.public_id.hex,
+            "body" : "Test Comment Body Updated"                  
+        }
+
+        client.force_authenticate(user = user)
+        response = client.put(self.endpoint + str(post.public_id) + "/comment/" + str(comment.public_id) + "/", data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['body'] == data["body"]
+
+    def test_delete(self, client, user, post, comment):
+        client.force_authenticate(user = user)
+        response = client.delete(self.endpoint + str(post.public_id) + "/comment/" + str(comment.public_id) + "/")
+        assert response.status_code == status.HTTP_204_NO_CONTENT
