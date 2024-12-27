bf6a2c6400352487bef446d996a12b21caef33cc
16211994 <cakek433@gmail.com>
2024-12-27 19:43:35 +0300

    some modification

--- core/user/tests/test_models.py
+++ core/user/tests/test_models.py
@@ -2,11 +2,11 @@ import pytest
 from core.user.models import User
 
 data_user = {
- "username": "test_user",
- "email": "test@gmail.com",
- "first_name": "Test",
- "last_name": "User",
- "password": "test_password"
+    "username": "test_user",
+    "email": "test@gmail.com",
+    "first_name": "Test",
+    "last_name": "User",
+    "password": "test_password"
 }
 
 @pytest.mark.django_db
