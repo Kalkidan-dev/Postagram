d79b5b6c7472df7f481476c23b8dda834eb77d9d
16211994 <cakek433@gmail.com>
2025-01-01 22:35:19 +0300

    Create a form for Login

--- social-media-app2/src/components/authentication/LoginForm.jsx
+++ social-media-app2/src/components/authentication/LoginForm.jsx
@@ -1,97 +1,81 @@
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
+import { useUserActions } from "../../hooks/user.actions";
 
 function LoginForm() {
-    const navigate = useNavigate();
     const [validated, setValidated] = useState(false);
-    const [form, setForm] = useState({});
+    const [form, setForm] = useState({
+        username: "",
+        password: "",
+    });
     const [error, setError] = useState(null);
+    const userActions = useUserActions();
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const loginForm = event.currentTarget;
 
         if (loginForm.checkValidity() === false) {
-            event.stopPropagation();
+        event.stopPropagation();
         }
 
         setValidated(true);
 
         const data = {
-            email: form.email,
-            password: form.password,
+        username: form.username,
+        password: form.password,
         };
 
-        axios
-            .post("http://localhost:5173/api/auth/login/", data)
-            .then((res) => {
-                localStorage.setItem(
-                    "auth",
-                    JSON.stringify({
-                        access: res.data.access,
-                        refresh: res.data.refresh,
-                        user: res.data.user,
-                    })
-                );
-                navigate("/");
-            })
-            .catch((err) => {
-                if (err.message) {
-                    setError(err.request.response);
-                }
-            });
+        userActions.login(data).catch((err) => {
+        if (err.message) {
+            setError(err.request.response);
+        }
+        });
     };
 
     return (
         <Form
-            id="registration-form"
-            className="border p-4 rounded"
-            noValidate
-            validated={validated}
-            onSubmit={handleSubmit}
+        id="registration-form"
+        className="border p-4 rounded"
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
         >
-            {/* Username input */}
-            <Form.Group className="mb-3">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    value={form.email}
-                    onChange={(e) => setForm({ ...form, email: e.target.value })}
-                    required
-                    type="text"
-                    placeholder="Enter email"
+        <Form.Group className="mb-3">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+                value={form.username}
+                onChange={(e) => setForm({ ...form, username: e.target.value })}
+                required
+                type="text"
+                placeholder="Enter username"
+            />
+            <Form.Control.Feedback type="invalid">
+            This file is required.
+            </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                value={form.password}
+                minLength="8"
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+                required
+                type="password"
+                placeholder="Password"
                 />
-                <Form.Control.Feedback type="invalid">
-                    This field is required.
-                </Form.Control.Feedback>
-            </Form.Group>
+            <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+            </Form.Control.Feedback>
+        </Form.Group>
 
-            {/* Password input */}
-            <Form.Group className="mb-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    value={form.password}
-                    minLength="8"
-                    onChange={(e) => setForm({ ...form, password: e.target.value })}
-                    required
-                    type="password"
-                    placeholder="Password"
-                />
-                <Form.Control.Feedback type="invalid">
-                    Please provide a valid password.
-                </Form.Control.Feedback>
-            </Form.Group>
+        <div className="text-content text-danger">{error && <p>{error}</p>}</div>
 
-            {/* Error message */}
-            <div className="text-content text-danger">
-                {error && <p>{error}</p>}
-            </div>
-
-            {/* Submit button */}
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
         </Form>
     );
 }
